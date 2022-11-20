62cae7154919cae593f247a0790ac70931d820d3
Dendy Armandiaz Aziz <dendy.aziz@gmail.com>
2022-11-20 19:10:08 +0700

    update: default layout. use the tailwind's native dark-mode

---
+++ layouts/default.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <Nuxt />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Default',
+  head () {
+    return {
+      htmlAttrs: {
+        class: 'dark'
+      },
+      bodyAttrs: {
+        class: 'antialiased text-slate-500 dark:text-slate-400 bg-white dark:bg-slate-900'
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
