4288ba5f1eb176e4e77d16c4d82b132150df1ec9
Dendy Armandiaz Aziz <dendy.aziz@gmail.com>
2022-11-21 17:25:55 +0700

    update: post actions

---
+++ components/post-action-button.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="h-[36px] hover:cursor-pointer rounded-md hover:bg-slate-100 dark:hover:bg-slate-700 flex items-center justify-center">
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+
+</style>
--- i18n/en.js
+++ i18n/en.js
@@ -4,4 +4,5 @@ export default {
   'Free to public': 'Free to public',
   'Add to your post': 'Add to your post',
   Current: 'Current',
+  Share: 'Share',
 }
