ed1359d8a957a2d7f44ae553a5caa89013cc8437
Dendy Armandiaz Aziz <dendy.aziz@gmail.com>
2022-11-23 22:07:08 +0700

    update: use smaller margin

--- components/post-form.vue
+++ components/post-form.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bg-white dark:bg-slate-800 sm:rounded-lg px-4 sm:px-6 py-4 ring-1 ring-slate-900/5 mb-2 md:mb-4">
+  <div class="bg-white dark:bg-slate-800 sm:rounded-md px-4 sm:px-6 py-4 ring-1 ring-slate-900/5 mb-2 md:mb-3">
     <div class="sm:flex">
       <!-- User -->
       <div class="flex sm:block items-center">
