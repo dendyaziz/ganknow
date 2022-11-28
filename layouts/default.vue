64e41f1bf2696fddbe34e56659652cd07b531670
Dendy Armandiaz Aziz <dendy.aziz@gmail.com>
2022-11-28 21:55:16 +0700

    update: sticky sidebar

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,7 +6,7 @@
     <div class="grid md:grid-cols-7 lg:grid-cols-8 xl:grid-cols-11 2xl:grid-cols-9 gap-4 mt-[61px] md:mx-4">
       <!-- Left sidebar -->
       <div class="xl:col-span-3 2xl:col-span-2 hidden xl:block sm:px-2 pt-2">
-        <Sidebar />
+        <Sidebar class="sticky top-[77px]" />
       </div>
       <!-- Main content -->
       <div class="md:col-span-4 lg:col-span-5 xl:col-span-5 2xl:col-span-5 pt-2 md:pt-4">
@@ -14,7 +14,7 @@
       </div>
       <!-- Right sidebar -->
       <div class="md:col-span-3 lg:col-span-3 xl:col-span-3 2xl:col-span-2 pt-2 md:pt-4 hidden md:block">
-        <suggestion />
+        <Suggestion class="sticky top-[77px]" />
       </div>
     </div>
   </div>
